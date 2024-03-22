e126c36286e365859de72c159a86f5dc1f4536ef
haoyuan <1161522231@qq.com>
2024-03-22 11:07:11 +0800

    面扫更新:1.改为上传图片 2.异常保护 3.状态回调

--- metashape/algm.py
+++ metashape/algm.py
@@ -1,18 +1,15 @@
 import asyncio
-import logging
-import time
-
-import Metashape
 import errno
+import logging
+import os
 import shutil
 import tempfile
 
-from sharement.sharedata import task_queue_dict
-from obs import ObsClient, Object
-import cv2
-import os
+import Metashape
+from obs import ObsClient
+from tornado import httpclient
 
-AVAILABLE = True
+from sharement.sharedata import task_queue_dict
 
 
 class ModelBuilder3D:
@@ -23,6 +20,12 @@ class ModelBuilder3D:
     )
 
     available = True
+
+    http_client = httpclient.AsyncHTTPClient()
+    bucket_url_map = {
+        "smilelink": "http://test.teethlink.cn/api/facescan",
+        "smilelink-prod": "https://teethlink.cn/api/facescan"
+    }
 
     def __init__(self):
         pass
@@ -36,21 +39,42 @@ class ModelBuilder3D:
         return f"patient/{patient_id}/facescan/{case_id}_{typ}_"
 
     @classmethod
+    def get_output_filepath(cls, user_id, case_id, typ, filename):
+        return f"doctor/{user_id}/facescan/{case_id}/models/facescan/{typ}/{filename}"
+
+    @classmethod
+    def make_request(cls, bucket, case_id, route):
+        return httpclient.HTTPRequest(
+            url=f"{cls.bucket_url_map.get(bucket)}/{case_id}/{route}",
+            method="PATCH",
+            connect_timeout=10,
+            allow_nonstandard_methods=True
+        )
+
+    @classmethod
     async def builder(cls):
         while True:
             # 接收新病例
             logging.info("prepare to get a new ill case")
             if "normal" not in task_queue_dict:
-                task_queue_dict["normal"] = asyncio.PriorityQueue()
+                task_queue_dict["normal"] = asyncio.Queue()
             _, information = await task_queue_dict["normal"].get()
             logging.info(f"now get a new ill case: {information}")
-            cls.available = False
 
             bucket = information.get("bucket")
             user_id = information.get("user_id")
             patient_id = information.get("patient_id")
             case_id = information.get("case_id")
             typ = information.get("type")
+
+            # 设置服务器和病例的状态
+            cls.available = False
+            try:
+                resp = await cls.http_client.fetch(cls.make_request(bucket, case_id, "executing"))
+            except Exception as e:
+                logging.error(f"error fetching: {e}")
+            else:
+                logging.info(f"resp: {resp.body}")
 
             try:
                 tmp_dir = tempfile.mkdtemp()  # create dir
@@ -59,8 +83,10 @@ class ModelBuilder3D:
                 # 读取图片
                 resp = cls.obsClient.listObjects(bucket, cls.get_input_prefix(user_id, case_id, typ))
                 if resp.status > 300:
-                    logging.error(f"read file form {user_id}/{case_id}/{typ} failed")
-                    continue
+                    raise Exception(f"read file form {user_id}/{case_id}/{typ} failed with {resp}")
+                # 判断图片张数是否符合要求
+                if len(resp.body.contents) == 0:
+                    raise Exception(f"numbers of {user_id}/{case_id}/{typ}'s images are empty")
                 for index, content in enumerate(resp.body.contents):
                     if content.key.endswith(".jpg"):
                         res = cls.obsClient.getObject(bucket, content.key,
@@ -98,28 +124,47 @@ class ModelBuilder3D:
 
                 # 导出obj模型
                 if chunk.model:
-                    chunk.exportModel(path + '/model/model.obj')
+                    chunk.exportModel(path + 'model/model.obj')
 
-                shutil.make_archive(path + "model", "zip", path + '/model/')
-                resp = cls.obsClient.putFile(bucket, cls.get_output_prefix(patient_id, case_id, typ) + "model.zip",
-                                             path + 'model.zip')
+                # shutil.make_archive(path + "model", "zip", path + '/model/')
+                # resp = cls.obsClient.putFile(bucket, cls.get_output_prefix(patient_id, case_id, typ) + "model.zip",
+                #                              path + 'model.zip')
+                # if resp.status >= 300:
+                #     raise Exception(f"upload zip file failed with resp: {resp}")
+
+                resp = cls.obsClient.putFile(bucket, cls.get_output_filepath(user_id, case_id, typ, "model.obj"),
+                                             path + 'model/model.obj')
+                logging.info(f"upload obj file resp: {resp}")
                 if resp.status >= 300:
-                    logging.error(f"upload zip file failed with resp: {resp}")
-
-                # resp = cls.obsClient.putFile(bucket, cls.get_output_prefix(patient_id, case_id, t) + "model.obj",
-                #                              path + 'model.obj')
-                # if resp.status >= 300:
-                #     logging.error("obj file upload failed..")
-                # resp = cls.obsClient.putFile(bucket, cls.get_output_prefix(patient_id, case_id, t) + "model.jpg",
-                #                              path + 'model.jpg')
-                # if resp.status >= 300:
-                #     logging.error("jpg file upload failed..")
-                # resp = cls.obsClient.putFile(bucket, cls.get_output_prefix(patient_id, case_id, t) + "model.mtl",
-                #                              path + 'model.mtl')
-                # if resp.status >= 300:
-                #     logging.error("mtl file upload failed..")
+                    raise Exception(f"upload obj file failed with resp: {resp}")
+                resp = cls.obsClient.putFile(bucket, cls.get_output_filepath(user_id, case_id, typ, "model.jpg"),
+                                             path + 'model/model.jpg')
+                logging.info(f"upload jpg file resp: {resp}")
+                if resp.status >= 300:
+                    raise Exception(f"upload jpg file failed with resp: {resp}")
+                resp = cls.obsClient.putFile(bucket, cls.get_output_filepath(user_id, case_id, typ, "model.mtl"),
+                                             path + 'model/model.mtl')
+                logging.info(f"upload mtl file resp: {resp}")
+                if resp.status >= 300:
+                    raise Exception(f"upload mtl file failed with resp: {resp}")
                 logging.info("Finished!")
 
+                # 设置病例的状态
+                try:
+                    resp = await cls.http_client.fetch(cls.make_request(bucket, case_id, "finished"))
+                except Exception as e:
+                    logging.error(f"error fetching: {e}")
+                else:
+                    logging.info(f"resp: {resp.body}")
+            except Exception as e:
+                logging.info(e)
+                # 设置病例的状态
+                try:
+                    resp = await cls.http_client.fetch(cls.make_request(bucket, case_id, "failed"))
+                except Exception as e:
+                    logging.error(f"error fetching: {e}")
+                else:
+                    logging.info(f"resp: {resp.body}")
             finally:
                 cls.available = True
                 try:
